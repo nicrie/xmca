2cebb13aad461c397da37accad1b4d699e490675
nrieger <niclasrieger@gmail.com>
2021-03-19 01:59:18 +0100

    add requirements.txt

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import setuptools
+from setuptools import setup, find_packages
 
 """
 Run the following code in your conda environment to make the package available
@@ -9,15 +9,25 @@ $ python setup.py develop
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+
+requirements = ['numpy',
+                'matplotlib',
+                'statsmodels',
+                'tqdm',
+                'cartopy']
+
+
+setup(
     name = "xmca",
+    include_package_data = True,
+    keywords = 'mca',
     author = "Niclas Rieger",
     author_email = "niclasrieger@gmail.com",
     description = "Maximum Covariance Analysis in Python",
     long_description = long_description,
     long_description_content_type = "text/markdown",
     url = "https://github.com/nicrie/xmca",
-    packages = setuptools.find_packages(),
+    packages = find_packages(),
     license = "GPL-3.0",
     classifiers = [
         "Programming Language :: Python :: 3",
@@ -26,4 +36,5 @@ setuptools.setup(
     python_requires='>=3.6',
     version_config = True,
     setup_requires=['setuptools-git-versioning'],
+    install_requirements = requirements,
 )
