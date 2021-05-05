a417e07324c7cf2977bd388d2a012a49b5090fcf
nrieger <niclasrieger@gmail.com>
2021-05-06 00:48:53 +0200

    some aesthetic changes

--- setup.py
+++ setup.py
@@ -20,26 +20,26 @@ requirements = ['numpy >= 1.19.2',
 
 
 setup(
-    name = "xmca",
-    include_package_data = True,
-    keywords = 'mca',
-    author = "Niclas Rieger",
-    author_email = "niclasrieger@gmail.com",
-    description = "Maximum Covariance Analysis in Python",
-    long_description = long_description,
-    long_description_content_type = "text/markdown",
-    url = "https://github.com/nicrie/xmca",
-    packages = find_packages(),
-    license = "GPL-3.0",
-    classifiers = [
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
+    name='xmca',
+    include_package_data=True,
+    keywords='mca',
+    author='Niclas Rieger',
+    author_email='niclasrieger@gmail.com',
+    description='Maximum Covariance Analysis in Python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/nicrie/xmca',
+    packages=find_packages(),
+    license='GPL-3.0',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
     version_config={
-        "dev_template": "{tag}.post{ccount}",
-        "dirty_template": "{tag}.post{ccount}"
+        'dev_template': '{tag}.post{ccount}',
+        'dirty_template': '{tag}.post{ccount}'
     },
-    setup_requires=['setuptools-git-versioning','numpy'],
-    install_requires = requirements,
+    setup_requires=['setuptools-git-versioning', 'numpy'],
+    install_requires=requirements,
 )
