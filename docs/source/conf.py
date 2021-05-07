1dc6adb0e9eb8ad4ae1e4c85bce50b127617fd16
nrieger <niclasrieger@gmail.com>
2021-05-07 16:10:13 +0200

    update conf.py to include installation of dependencies for cartopy

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,10 +13,16 @@
 import os
 import sys
 
-from subprocess import Popen, STDOUT, check_call
+import subprocess
 
 sys.path.insert(0, os.path.abspath('../../xmca/'))
 
+
+if os.environ.get('READTHEDOCS') == 'True':
+    # Install cartopy dependencies
+    subprocess.check_output([
+        'apt-get', 'install', '-y', 'libproj-dev', 'proj-data', 'proj-bin'
+    ])
 
 # -- Project information -----------------------------------------------------
 
@@ -33,8 +39,7 @@ release = '0.2.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.napoleon'
-]
+extensions = ['sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -56,10 +61,3 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-# little hack to enforce non-pip dependencies so that readthedocs builds correctly
-proc = Popen('apt-get install -y libproj-dev proj-data proj-bin', shell=True, stdin=None, stdout=open(os.devnull,"wb"), stderr=STDOUT, executable="/bin/bash")
-proc.wait()
-proc = Popen('apt-get install -y libgeos-dev', shell=True, stdin=None, stdout=open(os.devnull,"wb"), stderr=STDOUT, executable="/bin/bash")
-proc.wait()
-
